ac0dd9283fae80d9471ae4525c90a3b22b2a67a6
LongBOTT <93486974+LongBOTT@users.noreply.github.com>
2023-09-26 19:51:15 +0700

    set up

---
+++ src/main/java/com/supermarket/main/main.java
@@ -0,0 +1,10 @@
+package com.supermarket.main;
+
+public class main {
+        public main() {
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+}
