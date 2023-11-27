565ddc060fbbdf03eb51727a9f7a1ff240cf53cd
LongBOTT <93486974+LongBOTT@users.noreply.github.com>
2023-11-27 10:06:49 +0700

    Update Mini_supermarketManagement.java
    update login

--- src/main/java/com/supermarket/main/Mini_supermarketManagement.java
+++ src/main/java/com/supermarket/main/Mini_supermarketManagement.java
@@ -36,17 +36,12 @@ public class Mini_supermarketManagement {
         UIManager.put("TitlePane.iconSize", new Dimension(25, 25));
         UIManager.put("TitlePane.iconMargins", new Insets(3, 5, 0, 20));
 
-        homeGUI = new HomeGUI();
-        homeGUI.setAccount(new AccountBLL().getAccountList().get(0));
-        homeGUI.setVisible(true);
-//        loginGUI = new LoginGUI();
-//        CustomerBLL customerBLL = new CustomerBLL();
-//        customerBLL.addCustomer(new Customer(12, "abc", false, new Date(2,2,2023), "0963", false, new Date(2,2,2023), 0, false));
-//        System.out.println(customerBLL.getCustomerList());
-//        customerBLL.updateCustomer(new Customer(12, "xyz", false, new Date(2,2,2023), "0963", false, new Date(2,2,2023), 0, false));
-//        customerBLL.deleteCustomer(new Customer(12, "abc", false, new Date(2,2,2023), "0963", false, new Date(2,2,2023), 0, false));
-//        System.out.println(customerBLL.findCustomersBy(Map.of("id", 12)));
-//        System.out.println(customerBLL.findCustomers("id", "1"));
+        Thread thread = new Thread( () -> {
+            homeGUI = new HomeGUI();
+            homeGUI.setAccount(new AccountBLL().getAccountList().get(0));
+        });
+        thread.start();
+        loginGUI = new LoginGUI();
     }
 
     public static void exit(int status) {
